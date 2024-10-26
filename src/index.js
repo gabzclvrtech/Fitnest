7d6fa3994cf4d50d62fb88faed6c769438aed71b
gabzclvrtech <gabriel.tech94@gmail.com>
2024-10-26 20:59:07 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css';
+import GymSharingApp from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <GymSharingApp />
+  </React.StrictMode>
+);
