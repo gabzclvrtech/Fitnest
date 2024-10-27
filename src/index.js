fdc3aa0979a54e08232c439a95fdc0fc98cbfd6a
warbxbby <74873454+warbxbby@users.noreply.github.com>
2024-10-27 08:42:02 +0000

    installed packages, restructured file directory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './App.css';
-import GymSharingApp from './App';
+import './App.css'; // Add this line to import the CSS file
+import GymSharingApp from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
